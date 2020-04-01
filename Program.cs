44aa2d4c97b38aefc7297f6107623f71b12ac565
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-04-01 16:25:07 +0530

    Refcatored: Validating Email ID

--- Program.cs
+++ Program.cs
@@ -5,22 +5,49 @@ namespace UserRegistration
 {
     class Program
     {
-        static void Main(string[] args)
+        //FUNCTION TO VALIDATE FIRST AND LAST NAME
+        public static void validateName(string firstName, string lastName)
         {
-            Console.WriteLine("Enter First Name");
-            string firstName = Console.ReadLine();
-            Console.WriteLine("Enter Last Name");
-            string lastName = Console.ReadLine();
-
             Regex patternName = new Regex(@"^[A-Z]{1}[a-z]{2,}$");
+            
             if (patternName.IsMatch(firstName) && patternName.IsMatch(lastName))
             {
-                Console.WriteLine(" Name Is Valid ");
+                Console.WriteLine("Name Is Valid");
             }
             else
             {
                 Console.WriteLine("Name Is Not Valid");
             }
+        }
+
+        //FUNCTION TO VALIDATE EMAIL
+        public static void validateEmail(string email)
+        {
+            Regex patternEmail = new Regex(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$");
+
+            if (patternEmail.IsMatch(email))
+            {
+                Console.WriteLine("Email Is Valid");
+            }
+            else
+            {
+                Console.WriteLine("Email Is Not Valid");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //TAKING INPUTS
+            Console.WriteLine("Enter First Name");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter Email ID");
+            string email = Console.ReadLine();
+            
+            //VALIDATING FIRST NAME AND LAST NAME
+            Program.validateName(firstName,lastName);
+            Program.validateEmail(email);
         }
     }
 }
